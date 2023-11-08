c885b78d7d27c8dc79db3a24d86c18e1494d0659
catheriny09 <catheriny09@gmail.com>
2023-11-07 23:30:19 -0800

    added weather display to ui

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ class Weather_Frame(wx.Frame):
         
         super().__init__(parent=None, title='Weatherly')
         panel = wx.Panel(self)
+        
         sizer = wx.BoxSizer(wx.VERTICAL)
 
         self.text_ctrl = wx.TextCtrl(panel, size=(250, -1), style=wx.TE_CENTRE, pos=(5, 5))
@@ -18,9 +19,14 @@ class Weather_Frame(wx.Frame):
         
         button = wx.Button(panel, label='Select', pos=(5, 5))
         button.Bind(wx.EVT_BUTTON, self.on_press)
-        sizer.Add(button, 0, wx.ALL | wx.CENTER, 5)  
+        sizer.Add(button, 0, wx.ALL | wx.CENTER, 5) 
+        
+        self.w_text_ctrl = wx.TextCtrl(panel, size=(300, 300), style=wx.TE_READONLY, pos=(5,5)) 
+        sizer.Add(self.w_text_ctrl, 0, wx.ALL | wx.CENTER, 5)
         
         panel.SetSizer(sizer)
+        
+        self.CreateStatusBar()
 
         self.Show()
         
@@ -39,20 +45,19 @@ class Weather_Frame(wx.Frame):
             temp = "{0:.2f}".format(temp)
                 
             desc = data['weather'][0]['description']
-            print(f'Temperature: {temp} C')
-            print(f'Description: {desc}')
                 
             prompt = f'Temperature: {temp} C Description: {desc}\n'
                 
             response = openai.Completion.create(
                     engine="text-davinci-003",
-                    prompt=f"What should I wear in this weather?:{prompt}",
+                    prompt=f"What should I wear in this weather? Please give a short list:{prompt}",
                     max_tokens=1024,
                     temperature=0.5,
                 )
                 
-            output = response.choices[0].text
-            print(output)      
+            res = response.choices[0].text
+            output = (f'Temperature: {temp} C\nDescription: {desc}\n\n {res}')
+            self.w_text_ctrl.SetLabelText(output)
                 
         else:
             print('Error fetching weather data')
